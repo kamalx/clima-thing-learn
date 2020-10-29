a59fe6dfaa67efcd5fea7be92ad014f1fd9f4838
Kamal Morjal <kamal@morjal.com>
2020-10-30 04:21:05 +0530

    adding in the spinkit loader, and nav to location screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
+import 'package:Clima/screens/location_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:Clima/services/location.dart';
 import 'package:Clima/services/networking.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = 'f236d9844fdca387e028e2f84632550e';
 
@@ -33,10 +35,26 @@ class _LoadingScreenState extends State<LoadingScreen> {
     NetworkHelper nh = NetworkHelper(url);
 
     var weatherData = await nh.getData();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          return LocationScreen();
+        },
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
