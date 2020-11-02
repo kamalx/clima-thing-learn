7438a913a1db65a64352bb1fb5eb31af035af0fc
Kamal Morjal <kamal@morjal.com>
2020-11-02 05:52:38 +0530

    add new constant for input decoration: city field

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -18,4 +18,20 @@ const kButtonTextStyle = TextStyle(
 const kConditionTextStyle = TextStyle(
   fontSize: 90.0,
 );
+
+const kTextFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(
+    Icons.location_city,
+    color: Colors.white,
+  ),
+  hintText: 'Enter City Name',
+  hintStyle: TextStyle(color: Colors.grey),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10.0),
+    ),
+    borderSide: BorderSide.none,
+  ),
 );
