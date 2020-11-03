00fd5db5fca9c57085211d2df12ba80bb77afc48
Kamal Morjal <kamal@morjal.com>
2020-11-03 20:55:21 +0530

    set desired location accuracy to high

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -6,7 +6,7 @@ class Location {
   Future<void> getCurrentLocation() async {
     try {
       Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.low);
+          desiredAccuracy: LocationAccuracy.high);
       latitude = position.latitude;
       longitude = position.longitude;
     } catch (e) {
