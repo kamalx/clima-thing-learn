a479c962df96281163097b538e8fe0bd31f4ceef
Kamal Morjal <kamal@morjal.com>
2020-11-03 22:13:44 +0530

    fix call to Geolocator().getCurrentPosition

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -5,8 +5,8 @@ class Location {
 
   Future<void> getCurrentLocation() async {
     try {
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.high);
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
       latitude = position.latitude;
       longitude = position.longitude;
     } catch (e) {
